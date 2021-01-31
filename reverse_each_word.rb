c7010b2b6af36d1232aaf5afae88d91a4a132041
Amanda Nikrant <ajnikrant@gmail.com>
2021-01-31 19:07:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,13 +6,11 @@ def reverse_each_word(string)
 
   first_string = string.split
 
-  # while i < first_string.length
-  #   nested_string << first_string[i].reverse
-  #   i+=1
-  # end
-  # nested_string.collect {|e| e}
-
-nested_string << first_string.collect {|e| [e].reverse}
+  while i < first_string.length
+    nested_string << first_string[i].reverse
+    i+=1
+  end
+  nested_string.collect {|e| e}
 
 
   nested_string = nested_string.join (" ")
